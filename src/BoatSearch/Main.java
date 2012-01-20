9e7983539da647ca68a9e9476b52247eee08e066
Andrew Hurle <fracture91@gmail.com>
2012-01-20 05:03:54 -0500

    Read from and write to files

--- src/BoatSearch/Main.java
+++ src/BoatSearch/Main.java
@@ -1,21 +1,44 @@
 package BoatSearch;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		Set<Person> people = new HashSet<Person>();
-		people.add(new Person(10));
-		people.add(new Person(20));
-		people.add(new Person(30));
-		State initialState = new State(people, new HashSet<Person>(), false);
-		GraphSearch searcher = new GraphSearch(initialState);
-		System.out.print(searcher.findSolution());
+	public static void main(String[] args) throws IOException {
+		final Set<Person> people = new HashSet<Person>();
+		
+		final Scanner s = new Scanner(new File("input.txt"));
+
+		if(s.hasNextInt()) {
+			int length = s.nextInt();
+			while (length > 0 && s.hasNextInt()) {
+				people.add(new Person(s.nextInt()));
+				length--;
+			}
+		}
+		
+		s.close();
+		
+		final State initialState = new State(people, new HashSet<Person>(), false);
+		final GraphSearch searcher = new GraphSearch(initialState);
+		
+		final FileWriter output = new FileWriter("output.txt");
+		output.write("BFS " + searcher.findSolution());
+		output.close();
+		
+		//so looking at output is less painful
+		//http://www.adam-bien.com/roller/abien/entry/reading_a_file_into_a2
+		final String check = new Scanner(new File("output.txt")).useDelimiter("\\Z").next();
+        System.out.println("" + check);
 	}
 
 }
